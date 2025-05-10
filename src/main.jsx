13c3fced1dff6cd72ed088c4ff62c785a296de17
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2025-05-10 08:49:21 +0530

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import React from "react"
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Router>
     <App />
-  </Router>,
-)
+    <Analytics />
+  </Router>
+);
